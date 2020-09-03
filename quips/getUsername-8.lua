9d9db993c1c0dec4b3c7b7342cce1582b168cd43
Tracey Boydston <tracey@optimalprime.com>
2020-09-03 15:27:13 +0800

    Added lib funtion documentation

--- quips/getUsername-8.lua
+++ quips/getUsername-8.lua
@@ -30,7 +30,7 @@ quip.actions = {
     }, 
     {
         action="say",
-        value="It's always do difficult communicating with you primates."
+        value="It's always so difficult communicating with you primates."
     },
     {
         action="pause",
