722d3abeb27ff63f93112246012ea547ee57c206
Tracey Boydston <tracey@optimalprime.com>
2020-10-29 18:19:36 +0800

    Additional quips.

---
+++ quips/supremeWin-12.lua
@@ -0,0 +1,18 @@
+local quip = {
+    type = "supremeWin",
+}
+
+quip.actions = {
+    {
+        action="clearQuip",
+    },
+    {
+        action="say",
+        value="Was that as good for you as it was for me?"
+    },
+    {
+        action="end"
+    }
+}
+
+return quip
